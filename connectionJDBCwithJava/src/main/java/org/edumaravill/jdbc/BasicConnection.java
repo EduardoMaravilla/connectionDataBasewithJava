1d24bbf5a1e205b63a08ad018f935251819e4fd7
Eduardo Maravilla <eduardomaravillag@gmail.com>
2024-04-04 16:10:11 -0600

    BasicConnection

---
+++ connectionJDBCwithJava/src/main/java/org/edumaravill/jdbc/BasicConnection.java
@@ -0,0 +1,24 @@
+package org.edumaravill.jdbc;
+
+import java.sql.*;
+
+public class BasicConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/db_java_example";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement();
+            ResultSet resultSet = stmt.executeQuery("SELECT * FROM users");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("username"));
+            }
+            resultSet.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
